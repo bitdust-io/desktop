d07feb3d767bd993f21c4f2419efb25c9b491167
Veselin Penev <penev.veselin@gmail.com>
2019-07-21 14:25:31 +0200

    twisted to 19.2.1

--- app/scripts/deploy_win.bat
+++ app/scripts/deploy_win.bat
@@ -146,6 +146,8 @@ echo ##### Prepare virtual environment
 if %errorlevel% neq 0 goto DEPLOY_ERROR
 goto VenvOk
 :VenvUpdate
+rem TODO: this is slow and can fail if user is offline...
+rem this actually must be only executed when requirements.txt was changed
 echo ##### Update Python requirements
 %BITDUST_HOME%\venv\Scripts\pip.exe install -U -r %BITDUST_HOME%\src\requirements.txt
 if %errorlevel% neq 0 goto DEPLOY_ERROR
